bf10c1b67be65809f077283fe41c02872ecdab69
tiko manukyan <tikomanukyan44@gmail.com>
2021-03-04 13:43:42 +0400

    new buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,10 +13,11 @@ import { ServersComponent } from './servers/servers.component';
     ServerComponent,
     ServersComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,14 +5,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent implements OnInit {
-  title = 'My Card Title'
-  ImgUrl = 'https://cdn.auth0.com/blog/angular/logo3.png'
+export class ServersComponent {
+ public title = 'My Card Title';
+ public ImgUrl = 'https://cdn.auth0.com/blog/angular/logo3.png';
+ public disable = false;
+ public isServer = 'Server is not created';
+ public mouseOn = '';
 
-  ngOnInit() {
+
+
+
+  constructor() {
     setTimeout(() => {
-      this.ImgUrl = 'https://www.braveterry.com/wp-content/uploads/2015/12/vue.js.png'
-    }, 4000)
+      this.ImgUrl = 'https://www.braveterry.com/wp-content/uploads/2015/12/vue.js.png';
+      this.disable = true;
+    }, 4000);
   }
 
+createServer(event: any): void {
+    this.isServer = 'Server Created!'
+    }
+
+onMousecheck(event:any) {
+    this.mouseOn = 'You can type text';
+
 }
+
+mouseLeave(event:any) {
+    this.mouseOn = '';
+}
+
+
+ }
