6448e3632fef1af451a77d306b707654bf35d9f5
tiko manukyan <tikomanukyan44@gmail.com>
2021-03-24 18:08:09 +0400

    search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import {Book} from './book.model';
 export class AppComponent {
   // @ts-ignore
   public books: Book[] = JSON.parse(localStorage.getItem('book')) || [];
+  public searchResult: Book[];
 
   onBookCreated(book: Book): void {
     this.books.push(book);
@@ -20,4 +21,15 @@ export class AppComponent {
     localStorage.setItem('book', JSON.stringify(this.books));
 
   }
+
+  onSearch(event: any): void {
+    const search = event.target.value.toLowerCase();
+    this.searchResult = this.books.filter((book: Book) => {
+      if (book.title.toLowerCase().includes(search) || book.author.toLowerCase().includes(search) ) {
+        return true;
+      }
+      return false;
+    });
+  }
+
 }
