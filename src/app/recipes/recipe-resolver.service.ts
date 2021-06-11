e09636aa810f4be940dab6235d00b4c95654d50e
tiko manukyan <tikomanukyan44@gmail.com>
2021-06-11 18:56:24 +0400

    change project

---
+++ src/app/recipes/recipe-resolver.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import {Recipe} from './recipe.model';
+import {DataStorageService} from '../shared/data-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeResolverService implements Resolve<Recipe[]> {
+
+  constructor(private dataStorageService: DataStorageService) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): any {
+    return this.dataStorageService.fetchRecipes()
+
+  }
+
+}
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import {Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
-import {map} from 'rxjs/operators';
+import {map, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -20,17 +20,16 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>('https://ng-course-recipe-book-adccb-default-rtdb.firebaseio.com/recipe.json')
+    return this.http.get<Recipe[]>('https://ng-course-recipe-book-adccb-default-rtdb.firebaseio.com/recipe.json')
       .pipe(map( recipes => {
         return recipes.map((recipe) => {
           return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
         })
-      }))
-      .subscribe((recipes) => {
-        this.recipeService.setRecipes(recipes)
-        console.log(recipes)
-      })
+      }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes)
+        })
+      )
   }
-
 
 }
